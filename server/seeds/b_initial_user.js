a80f7ba63ad5a074922f0d77a9fa4b9e7312ce10
mojo9492 <mojo9492@outlook.com>
2021-08-26 14:58:10 -0700

    Add feature

--- server/seeds/b_initial_user.js
+++ server/seeds/b_initial_user.js
@@ -1,14 +1,14 @@
 const faker = require('faker');
 
 const createFakeUser = () => ({
-  username: faker.animal.cetacean(),
+    username: faker.animal.cetacean(),
 });
 
 const fakeUsers = [];
 
 for (let i = 0; i < 30; i += 1) {
-  fakeUsers.push(createFakeUser());
+    fakeUsers.push(createFakeUser());
 }
 
 exports.seed = (knex) => knex('users').del()
-  .then(() => knex('users').insert(fakeUsers));
+    .then(() => knex('users').insert(fakeUsers));
