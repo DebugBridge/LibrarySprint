878d009eb1c2ad52391215cfddc2c5a67796bfd5
mojo9492 <mojo9492@outlook.com>
2021-08-26 11:17:13 -0700

    Add e2e test for header. add header

--- client/cypress/integration/home/home.spec.js
+++ client/cypress/integration/home/home.spec.js
@@ -1,7 +1,13 @@
+/* eslint-disable no-undef */
 /// <reference types="cypress" />
+
 describe('library', () => {
-  beforeEach(() => {
-    cy.visit('/');
-    cy.viewport(1500, 1500);
-  });
+    beforeEach(() => {
+        cy.visit('/');
+        cy.viewport(1500, 1500);
+    });
+
+    it('displays a header', () => {
+        cy.get('[data-cy="header"]');
+    });
 });
--- client/src/components/book/Book.jsx
+++ client/src/components/book/Book.jsx
@@ -3,23 +3,23 @@ import PropTypes from 'prop-types';
 import './Book.sass';
 
 const Book = ({ title, author, isbn }) => (
-  <ul className="book-list">
-    <li>{title}</li>
-    <li>
-      By:&nbsp;
-      {author}
-    </li>
-    <li>
-      ISBN:&nbsp;
-      {isbn}
-    </li>
-  </ul>
+    <ul className='book-list'>
+        <li>{title}</li>
+        <li>
+            By:&nbsp;
+            {author}
+        </li>
+        <li>
+            ISBN:&nbsp;
+            {isbn}
+        </li>
+    </ul>
 );
 
 Book.propTypes = {
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-  isbn: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    isbn: PropTypes.string.isRequired,
 };
 
 export default Book;
